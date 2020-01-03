d52321b4694e4b57bc08c19b426c924d395e4fa5
Cj <cchiass2@oswego.edu>
2020-01-03 12:51:10 -0500

    2nd Commit

--- Scraper.py
+++ Scraper.py
@@ -1,9 +1,8 @@
 import os
-
-from bs4 import BeautifulSoup
-
-
-
+import csv
+import pyautogui
+import time
+import pyperclip
 
 def removeBlankPages(dir):
 
@@ -36,23 +35,86 @@ def pullIDs(dir):
 
     for i in arr:
 
+        #if i is not a directory and if i is not have the file extension .py or csv
+
+
+
         #swap this out on server
-        #if ".py" or ".csv" not in i:
-        if ".html" in i:# or just have all files in the folder be what needs to be parsed
-            print(i)
-            lines = list(open(i,"r"))
+        #if ".html" in i:# or just have all files in the folder be what needs to be parsed
+        #if i is  os.path.isdir(i) or ".py" not in i or ".csv" not in i:
+        if os.path.isdir(i) is False:
+            if ".py" not in i:
+                if ".csv" not in i:
+                    print(i)
+                    lines = list(open(i,"r"))
 
-            for line in lines:
-                if '<a href="offenderdetails.php?OfndrID=' in line and 'img src' not in line:
-                    x = line.index('OfndrID=')
-                    y = line.index('&AgencyID')
+                    for line in lines:
+                        if '<a href="offenderdetails.php?OfndrID=' in line and 'img src' not in line:
+                            x = line.index('OfndrID=')
+                            y = line.index('&AgencyID')
 
-                    IDset.add(line[x+8:y]+',')
+                            IDset.add(line[x+8:y]+',')
 
     for j in IDset:
         csv.write(j)
 
-pullIDs("/home/cj/PycharmProjects/OhioScraper2.0/")
+#pullIDs("/home/cj/PycharmProjects/OhioScraper2.0/")
 
 
 
+def LookupandSaveHTML(id):
+    print('[Current ID]'+id)
+    #http://www.icrimewatch.net/offenderdetails.php?OfndrID=1571559&AgencyID=55149
+    #http://www.icrimewatch.net/offenderdetails.php?OfndrID=
+    pyperclip.copy('http://www.icrimewatch.net/offenderdetails.php?OfndrID='+str(id)+'&AgencyID=55149')
+    screenWidth, screenHeight = pyautogui.size()
+    #print(pyautogui.size())
+    currentMouseX, currentMouseY = pyautogui.position()
+   # print(pyautogui.position())
+    pyautogui.moveTo(599, 111)
+    pyautogui.doubleClick()
+    pyautogui.press('backspace')
+    pyautogui.hotkey('ctrl','v')
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.hotkey('ctrl','s')
+    pyperclip.copy(id)
+    pyautogui.press('backspace')
+    time.sleep(3)
+    #pyperclip.paste(id)
+    pyautogui.hotkey('ctrl','v')
+    time.sleep(1)
+    pyautogui.press('enter')
+    time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+def getImages():
+
+    with open('IDs.csv', 'r') as f:
+        # check LastLocal.txt for place in CSV file
+        #   placeholder = open('LastLocal.txt','r')
+
+      #  placeholderint = placeholder.readline()
+
+        reader = csv.reader(f)
+        #size = list
+
+        for i in reader:#turns reader into a iterable list
+            for j in range(len(i)):#iterates through the cell rows
+                LookupandSaveHTML(i[j])
+
+
+
+
+time.sleep(3)
+
+
+getImages()
